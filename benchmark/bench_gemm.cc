5a5f78b48f32a288e976a6dd86909598a992bb27
friden <Zplearner@163.com>
2024-06-23 19:36:03 +0800

    add cpu gemm transport benchmark

--- benchmark/bench_gemm.cc
+++ benchmark/bench_gemm.cc
@@ -5,15 +5,28 @@
 #include "gemm/cpu_gemm.hpp"
 #include "utilities/generate_matrix.hpp"
 
+static void BM_cpu_transport_native_gemm(benchmark::State& state) {
+    float lower_bound = 0.0;
+    float upper_bound = 1.0;
+
+    auto A = utilities::generate_random_matrix<float, 2>(lower_bound, upper_bound, {state.range(0), state.range(1)});
+    auto B = utilities::generate_random_matrix<float, 2>(lower_bound, upper_bound, {state.range(0), state.range(1)});
+
+    std::vector<std::vector<float>> C(A.size(), std::vector<float>(B.size(), 0.0f));
+
+    for (auto _ : state) {
+        gemm::cpu_native_gemm(A, B, C, true);
+        benchmark::DoNotOptimize(C);
+    }
+}
+
 // Native GEMM
 static void BM_cpu_native_gemm(benchmark::State& state) {
     float lower_bound = 0.0;
     float upper_bound = 1.0;
 
-    auto A = utilities::generate_random_matrix<float, 2>(lower_bound, upper_bound,
-                                                         {state.range(0), state.range(1)});
-    auto B = utilities::generate_random_matrix<float, 2>(lower_bound, upper_bound,
-                                                         {state.range(1), state.range(0)});
+    auto A = utilities::generate_random_matrix<float, 2>(lower_bound, upper_bound, {state.range(0), state.range(1)});
+    auto B = utilities::generate_random_matrix<float, 2>(lower_bound, upper_bound, {state.range(1), state.range(0)});
 
     std::vector<std::vector<float>> C(A.size(), std::vector<float>(B[0].size(), 0.0f));
 
@@ -23,15 +36,28 @@ static void BM_cpu_native_gemm(benchmark::State& state) {
     }
 }
 
+static void BM_cpu_transport_openmp_gemm(benchmark::State& state) {
+    float lower_bound = 0.0;
+    float upper_bound = 1.0;
+
+    auto A = utilities::generate_random_matrix<float, 2>(lower_bound, upper_bound, {state.range(0), state.range(1)});
+    auto B = utilities::generate_random_matrix<float, 2>(lower_bound, upper_bound, {state.range(0), state.range(1)});
+
+    std::vector<std::vector<float>> C(A.size(), std::vector<float>(B.size(), 0.0f));
+
+    for (auto _ : state) {
+        gemm::cpu_openmp_gemm(A, B, C, true);
+        benchmark::DoNotOptimize(C);
+    }
+}
+
 // OpenMP GEMM
 static void BM_cpu_openmp_gemm(benchmark::State& state) {
     float lower_bound = 0.0;
     float upper_bound = 1.0;
 
-    auto A = utilities::generate_random_matrix<float, 2>(lower_bound, upper_bound,
-                                                         {state.range(0), state.range(1)});
-    auto B = utilities::generate_random_matrix<float, 2>(lower_bound, upper_bound,
-                                                         {state.range(1), state.range(0)});
+    auto A = utilities::generate_random_matrix<float, 2>(lower_bound, upper_bound, {state.range(0), state.range(1)});
+    auto B = utilities::generate_random_matrix<float, 2>(lower_bound, upper_bound, {state.range(1), state.range(0)});
 
     std::vector<std::vector<float>> C(A.size(), std::vector<float>(B[0].size(), 0.0f));
 
@@ -41,16 +67,30 @@ static void BM_cpu_openmp_gemm(benchmark::State& state) {
     }
 }
 
+static void BM_cpu_transport_parallel_for_each_gemm(benchmark::State& state) {
+    float lower_bound = 0.0;
+    float upper_bound = 1.0;
+
+    auto A = utilities::generate_random_matrix<float, 2, int64_t>(lower_bound, upper_bound, {state.range(0), state.range(1)});
+
+    auto B = utilities::generate_random_matrix<float, 2, int64_t>(lower_bound, upper_bound, {state.range(0), state.range(1)});
+
+    std::vector<std::vector<float>> C(A.size(), std::vector<float>(B.size(), 0.0f));
+
+    for (auto _ : state) {
+        gemm::cpu_std_parallel_gemm(A, B, C, true);
+        benchmark::DoNotOptimize(C);
+    }
+}
+
 // Parallel std::for_each GEMM
 static void BM_cpu_parallel_for_each_gemm(benchmark::State& state) {
     float lower_bound = 0.0;
     float upper_bound = 1.0;
 
-    auto A = utilities::generate_random_matrix<float, 2, int64_t>(lower_bound, upper_bound,
-                                                                  {state.range(0), state.range(1)});
+    auto A = utilities::generate_random_matrix<float, 2, int64_t>(lower_bound, upper_bound, {state.range(0), state.range(1)});
 
-    auto B = utilities::generate_random_matrix<float, 2, int64_t>(lower_bound, upper_bound,
-                                                                  {state.range(1), state.range(0)});
+    auto B = utilities::generate_random_matrix<float, 2, int64_t>(lower_bound, upper_bound, {state.range(1), state.range(0)});
 
     std::vector<std::vector<float>> C(A.size(), std::vector<float>(B[0].size(), 0.0f));
 
@@ -60,8 +100,11 @@ static void BM_cpu_parallel_for_each_gemm(benchmark::State& state) {
     }
 }
 
+BENCHMARK(BM_cpu_transport_native_gemm)->Args({1024, 2048})->Unit(benchmark::kMillisecond);
 BENCHMARK(BM_cpu_native_gemm)->Args({1024, 2048})->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_cpu_transport_openmp_gemm)->Args({1024, 2048})->Unit(benchmark::kMillisecond);
 BENCHMARK(BM_cpu_openmp_gemm)->Args({1024, 2048})->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_cpu_transport_parallel_for_each_gemm)->Args({1024, 2048})->Unit(benchmark::kMillisecond);
 BENCHMARK(BM_cpu_parallel_for_each_gemm)->Args({1024, 2048})->Unit(benchmark::kMillisecond);
 
 BENCHMARK_MAIN();
--- include/gemm/cpu_gemm.hpp
+++ include/gemm/cpu_gemm.hpp
@@ -2,26 +2,36 @@
 
 #include <algorithm>
 #include <execution>
+#include <iostream>
 #include <numeric>
 #include <vector>
-
 namespace gemm {
 
 template <typename T>
-void cpu_native_gemm(const std::vector<std::vector<T>>& A, const std::vector<std::vector<T>>& B,
-                     std::vector<std::vector<T>>& C) {
-    for (int i = 0; i < A.size(); i++) {
-        for (int j = 0; j < B[0].size(); j++) {
-            for (int k = 0; k < B.size(); k++) {
-                C[i][j] += A[i][k] * B[k][j];
+void cpu_native_gemm(const std::vector<std::vector<T>>& A, const std::vector<std::vector<T>>& B, std::vector<std::vector<T>>& C,
+                     bool transpose_b = false) {
+    if (!transpose_b) {
+        for (int i = 0; i < A.size(); i++) {
+            for (int j = 0; j < B[0].size(); j++) {
+                for (int k = 0; k < B.size(); k++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+    } else {
+        for (int i = 0; i < A.size(); i++) {
+            for (int j = 0; j < B.size(); j++) {
+                for (int k = 0; k < B[0].size(); k++) {
+                    C[i][j] += A[i][k] * B[j][k];
+                }
             }
         }
     }
 }
 
 template <typename T>
-void cpu_openmp_gemm(const std::vector<std::vector<T>>& A, const std::vector<std::vector<T>>& B,
-                     std::vector<std::vector<T>>& C) {
+void cpu_openmp_gemm(const std::vector<std::vector<T>>& A, const std::vector<std::vector<T>>& B, std::vector<std::vector<T>>& C,
+                     bool transpose_b = false) {
     // Using collapse(2) combines the outer two loops (i and j) into a single parallel loop.
     // This approach allows each thread to handle the computation of a row or block of rows in the
     // output matrix C. It strikes a balance between maximizing parallelism and minimizing overhead
@@ -30,28 +40,59 @@ void cpu_openmp_gemm(const std::vector<std::vector<T>>& A, const std::vector<std
     // collapse(3) can potentially increase parallelism, it often leads to significant overhead from
     // thread management and synchronization, especially when the computation per element is
     // relatively small.
+    if (!transpose_b) {
 #pragma omp parallel for collapse(2)
-    for (int i = 0; i < A.size(); i++) {
-        for (int j = 0; j < B[0].size(); j++) {
-            for (int k = 0; k < B.size(); k++) {
-                C[i][j] += A[i][k] * B[k][j];
+        for (int i = 0; i < A.size(); i++) {
+            for (int j = 0; j < B[0].size(); j++) {
+                T local_element = 0;
+                for (int k = 0; k < B.size(); k++) {
+                    local_element += A[i][k] * B[k][j];
+                }
+                C[i][j] = local_element;
+            }
+        }
+    } else {
+#pragma omp parallel for collapse(2)
+        for (int i = 0; i < A.size(); i++) {
+            for (int j = 0; j < B.size(); j++) {
+                T local_element = 0;
+                for (int k = 0; k < B[0].size(); k++) {
+                    local_element += A[i][k] * B[j][k];
+                }
+                C[i][j] = local_element;
             }
         }
     }
 }
 
 template <typename T>
-void cpu_std_parallel_gemm(const std::vector<std::vector<T>>& A,
-                           const std::vector<std::vector<T>>& B, std::vector<std::vector<T>>& C) {
-    std::for_each(std::execution::par, C.begin(), C.end(), [&](std::vector<T>& row) {
-        size_t i = &row - &C[0];
-        std::for_each(std::execution::par, row.begin(), row.end(), [&](T& element) {
-            size_t j = &element - &row[0];
-            for (int k = 0; k < B.size(); ++k) {
-                element += A[i][k] * B[k][j];
-            }
+void cpu_std_parallel_gemm(const std::vector<std::vector<T>>& A, const std::vector<std::vector<T>>& B, std::vector<std::vector<T>>& C,
+                           bool transpose_b = false) {
+    if (!transpose_b) {
+        std::for_each(std::execution::par, C.begin(), C.end(), [&](std::vector<T>& row) {
+            size_t i = &row - &C[0];
+            std::for_each(std::execution::par, row.begin(), row.end(), [&](T& element) {
+                size_t j = &element - &row[0];
+                T local_element = 0;
+                for (int k = 0; k < B.size(); ++k) {
+                    local_element += A[i][k] * B[k][j];
+                }
+                element = local_element;
+            });
         });
-    });
+    } else {
+        std::for_each(std::execution::par, C.begin(), C.end(), [&](std::vector<T>& row) {
+            size_t i = &row - &C[0];
+            std::for_each(std::execution::par, row.begin(), row.end(), [&](T& element) {
+                size_t j = &element - &row[0];
+                T local_element = 0;
+                for (int k = 0; k < B[0].size(); ++k) {
+                    local_element += A[i][k] * B[j][k];
+                }
+                element = local_element;
+            });
+        });
+    }
 }
 
 }  // namespace gemm
